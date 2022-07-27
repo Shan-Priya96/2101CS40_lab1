8c2b9c7d34605dab4bb1d022b9b9efc44432a9a6
Shan-Priya96 <94847071+Shan-Priya96@users.noreply.github.com>
2022-07-27 18:51:26 +0530

    9th commit, added selection sort function

--- sorting.c
+++ sorting.c
@@ -28,6 +28,22 @@ void insertionSort(int array[], int length) {
     array[j + 1] = key;
   }
 }
+//selection sort
+void selectionSort(int array[], int length) {
+  for (int step = 0; step < length - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < length; i++) {
+
+      // To sort in descending order, change > to < in this line.
+      // Select the minimum element in each loop.
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+  }
+}
 
 int main() {
   
